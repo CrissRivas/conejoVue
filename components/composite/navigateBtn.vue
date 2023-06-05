bd38d43f3cec6e92ffd34e58768a009ba95c1a3a
CrissRivas <cristopher_rivas@outlook.es>
2023-06-05 04:15:26 -0600

    testing upload

---
+++ components/composite/navigateBtn.vue
@@ -0,0 +1,33 @@
+<template>
+   <v-row justify="center" align="center" class="my-4 mx-1 titleBar">  
+        <v-col align="center" class="cube">
+            <h2 class="cube">{{titleName}}</h2>
+        </v-col>
+   </v-row>
+</template>
+
+<script>
+    export default {
+        props:  {
+            titleName:  {
+                type: Array,
+			    required: true
+            }
+        }
+        
+    }
+</script>
+
+<style>
+.titleBar{
+    border: 3px solid #1F1F1F;
+    color: #8B956D;
+    background: #4D533C;
+    box-shadow: 3px 3px #4D533C;
+}
+.cube{
+        color: #8B956D;
+        background: #4D533C;
+        text-shadow: 2px 2px #1F1F1F;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <v-row >
-    
+  <v-row>
+    <v-col>
+      <v-row justify="center" align="center" class="pa-4 my-4">
+        <img src="@/assets/gif/w.gif" height="50px" class="mx-2" />
+        <img src="@/assets/png/cris.png" class="mx-2" height="120px" />
+        <img src="@/assets/png/c.png" height="50px" class="mx-2"/>
+      </v-row>
+      <v-row>
+        <v-col>
+          <router-link to="/uxui">
+            <navigateBtn titleName="UX / UI"/>
+          </router-link>
+          <router-link to="/frontend">
+            <navigateBtn titleName="Frontend"/>
+          </router-link>
+          <router-link to="/backend">
+            <navigateBtn titleName="Backend"/>
+          </router-link>
+          <router-link to="/devops">
+            <navigateBtn titleName="DevOps"/>
+          </router-link>
+        </v-col>
+      </v-row>
+    </v-col>
   </v-row>
 </template>
 
 <script>
+import navigateBtn from "~/components/composite/navigateBtn.vue";
+
 export default {
+  components:{
+    navigateBtn
+  },
+  data() {
+    return {
+      crisImg: require('~/assets/png/cris.png')
+    }
+  },
   
 }
 </script>
